4191a1b7732969b2d0ccbe8bb36019dd444f6078
shern <eshern@gmail.com>
2023-01-30 16:04:15 +0800

    First Commit

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash 
+
+#above is so called "shebang line"
+
+echo "Hello World!!" 
+
+# cmd: chmod +x hello.sh --> make the file executable, 
+# so that all users can execute the file, see this enabled by cmd: ls -l hello.sh
+# -rwxrwxrwx 1 root root 53 Jan 30 11:22 hello.sh
+# instead of 
+# -rw-rw-rw- 1 root root 53 Jan 30 11:22 hello.sh
